661b75341e617ccfc00001f99cc6e314afdad6eb
Vu Quoc Khanh <173862940+vqk-2003@users.noreply.github.com>
2025-11-13 22:07:13 +0700

    Add some keymaps for Neovim config

---
+++ .config/nvim/lua/core/keymaps.lua
@@ -0,0 +1,10 @@
+-- -- Clear highlights on search when pressing <Esc> in normal mode
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+
+-- Resize window using <ctrl> arrow keys
+vim.keymap.set("n", "<C-Up>", "<cmd>resize +2<cr>", { desc = "Increase Window Height" })
+vim.keymap.set("n", "<C-Down>", "<cmd>resize -2<cr>", { desc = "Decrease Window Height" })
+vim.keymap.set("n", "<C-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease Window Width" })
+vim.keymap.set("n", "<C-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase Window Width" })
+
+

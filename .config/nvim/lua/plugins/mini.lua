82f067de03a7997c25235d4b725c2e8663b7e75e
Vu Quoc Khanh <173862940+vqk-2003@users.noreply.github.com>
2025-11-14 15:59:02 +0700

    Add mini.pairs

--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -1,6 +1,10 @@
 return {
   "echasnovski/mini.nvim",
   config = function()
+    -- mini.notify
     require("mini.notify").setup()
+
+    -- mini.pairs
+    require("mini.pairs").setup()
   end,
 }

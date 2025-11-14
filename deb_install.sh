1eb4deaaa1cb24d37fb31283cab28c5b584e28fb
Vu Quoc Khanh <173862940+vqk-2003@users.noreply.github.com>
2025-11-14 10:36:52 +0700

    Add cmake, ninja to essential packages

--- deb_install.sh
+++ deb_install.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
 pkgs=(
+  # Basic development tools
   build-essential
+  # Get online packages
   curl
+  # Version control
   git
+  # For Neovim clipboard
   xclip
+  # Better grep and find
   ripgrep
   fd-find
+  # Terminal multiplexer
   tmux
+  # C/C++ development tools
+  cmake
+  ninja-build
 )
 
 apt-get update
